137b8db6b11b83daf868e57a579d33be1627283e
rmn2520 <ramonuhuyy@gmail.com>
2024-09-07 09:50:17 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Pesan di console saat halaman dimuat
-console.log("Halaman perkenalan berhasil dimuat!");
-
-// Fungsi animasi tambahan jika dibutuhkan di masa depan
-function showWelcomeMessage() {
-    alert("Selamat datang di halaman saya!");
-}
